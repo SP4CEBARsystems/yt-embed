b10a3a9973f29b7db6a3acc03cfc801acaf1c81f
Boaz Crezee <bjcrezee@gmail.com>
2026-01-16 00:55:21 +0100

    removed unnecessary iframe testing parameters from EmbedMaker

--- js/EmbedMaker.js
+++ js/EmbedMaker.js
@@ -52,10 +52,7 @@ export default class EmbedMaker {
 
         iframe.id = 'youtubePlayer';
         iframe.src = src;
-        iframe.title = "lofi hip hop radio 📚 beats to relax/study to";
-        iframe.frameBorder = "0";
         iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-        iframe.referrerPolicy = "strict-origin-when-cross-origin";
         iframe.allowFullscreen = true;
 
         return iframe;
