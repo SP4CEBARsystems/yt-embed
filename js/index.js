ba31d70ef63d174a36eb3f2322067d6d43556a9b
Boaz Crezee <bjcrezee@gmail.com>
2026-01-15 18:34:06 +0100

    add loading from youtube urls

--- js/index.js
+++ js/index.js
@@ -13,25 +13,53 @@ function getQueryParam(name) {
     return params.get(name);
 }
 
-function loadVideo() {
-    const videoId = getQueryParam("v");
-    const app = document.getElementById("app");
-    
-    if (videoId) {
-        const container = document.createElement("div");
-        container.className = "container";
-        
-        const iframe = document.createElement("iframe");
-        iframe.src = `https://www.youtube.com/embed/${encodeURIComponent(videoId)}`;
-        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-        iframe.allowFullscreen = true;
-        
-        container.appendChild(iframe);
-        app.appendChild(container);
-    } else {
-        const error = document.createElement("div");
-        error.className = "error";
-        error.textContent = 'Missing "v" parameter in the URL.';
-        app.appendChild(error);
-    }
+/**
+ * 
+ * @param {string} url 
+ * @returns 
+ */
+function extractVideoId(url) {
+    const matchVideoId = /(?:youtube\.com\/(?:watch\?.*v=|embed\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(matchVideoId);
+    return match ? match[1] : null;
 }
+
+function loadVideo() {
+    const videoUrl = getQueryParam("u");
+    const app = document.getElementById("app");
+    let videoId;
+    if (videoUrl) {
+        videoId = extractVideoId(videoUrl) ?? getQueryParam("v");
+    }
+
+    if (!app) {
+        return;
+    } else if (!videoId) {
+        showError(app, 'Missing "v" parameter in the URL.');
+        return;
+    }
+    
+    const container = document.createElement("div");
+    container.className = "container";
+    
+    const iframe = document.createElement("iframe");
+    iframe.src = `https://www.youtube.com/embed/${encodeURIComponent(videoId)}`;
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
+    
+    container.appendChild(iframe);
+    app.appendChild(container);
+}
+
+/**
+ * 
+ * @param {HTMLElement} app 
+ * @param {string} message 
+ */
+function showError(app, message) {
+    const error = document.createElement("div");
+    error.className = "error";
+    error.textContent = message;
+    app.appendChild(error);
+}
+
